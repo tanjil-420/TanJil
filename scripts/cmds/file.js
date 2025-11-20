32a7bbb98baa96183a1cdc43d7ab3822dc94e3ca
TanJil Hasan <jodiidaseplz@gmail.com>
2025-11-20 20:55:30 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["61579509758592","61581027592796"];
+		const permission = ["61583848689101"];
 		if (!permission.includes(event.senderID)) {
 			return api.sendMessage(" You don't have permission to use this command. 🐤", event.threadID, event.messageID);
 		}
