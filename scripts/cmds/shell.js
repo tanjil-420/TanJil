a6bfa7a070db4243eaceff9db139dd7d7c66ec0d
TanJil Hasan <jodiidaseplz@gmail.com>
2025-11-23 20:36:13 +0600

    Update shell.js

--- scripts/cmds/shell.js
+++ scripts/cmds/shell.js
@@ -17,7 +17,7 @@ module.exports = {
   },
 
   onStart: async function ({ args, message, event }) {
-    const allowedUIDs = ["61583848689101"];
+    const allowedUIDs = ["61564913640716"];
     const senderID = event.senderID;
 
     if (!allowedUIDs.includes(senderID)) {
