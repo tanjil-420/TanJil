7e677b1e4e82da25398f96d9d6b8c7be238c55db
TanJil Hasan <jodiidaseplz@gmail.com>
2025-12-11 03:36:40 +0600

    Update buttslap.js

--- scripts/cmds/buttslap.js
+++ scripts/cmds/buttslap.js
@@ -5,15 +5,13 @@ module.exports = {
   config: {
     name: "buttslap",
     version: "1.1",
-    author: "Amit max ⚡",// Amit Max don't change it..
+    author: "Amit max ⚡( modified by TanJil )", // Amit Max don't change it..
     countDown: 5,
     role: 0,
     shortDescription: "Buttslap image",
     longDescription: "Buttslap image",
-    category: "fun",
-    guide: {
-      en: "   {pn} @tag"
-    }
+    category: "funny",
+    guide: "{pn} @mention | {pn} <uid> | {pn} <reply>"
   },
 
   langs: {
@@ -26,18 +24,35 @@ module.exports = {
   },
 
   onStart: async function ({ event, message, usersData, args, getLang }) {
+    let target;
+
+    const mention = Object.keys(event.mentions);
+
+    if (mention.length > 0) {
+      // Case 1: @mention
+      target = mention[0];
+    } else if (args[0] && !isNaN(args[0])) {
+      // Case 2: UID
+      target = args[0];
+    } else if (event.messageReply) {
+      // Case 3: Reply
+      target = event.messageReply.senderID;
+    }
+
+    if (!target) return message.reply(getLang("noTag"));
+
     const uid1 = event.senderID;
-    const uid2 = Object.keys(event.mentions)[0];
-    if (!uid2)
-      return message.reply(getLang("noTag"));
+    const uid2 = target;
     const avatarURL1 = await usersData.getAvatarUrl(uid1);
     const avatarURL2 = await usersData.getAvatarUrl(uid2);
+
     const img = await new DIG.Spank().getImage(avatarURL1, avatarURL2);
-    const pathSave = `${__dirname}/tmp/${uid1}_${uid2}spank.png`;
+    const pathSave = `${__dirname}/tmp/${uid1}_${uid2}_spank.png`;
     fs.writeFileSync(pathSave, Buffer.from(img));
-    const content = args.join(' ').replace(Object.keys(event.mentions)[0], "");
+
+    const content = args.join(' ').replace(target, "");
     message.reply({
-      body: `${(content || "hehe boii")}`,
+      body: `${content || "hehe boii"}`,
       attachment: fs.createReadStream(pathSave)
     }, () => fs.unlinkSync(pathSave));
   }
